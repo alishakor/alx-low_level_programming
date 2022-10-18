4f67fa0e627f02cbbdac20158876bf4fe97accb0
alishakor <aliuokeowo@gmail.com>
2022-10-18 16:26:03 +0000

    Print & Size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ /**
+   * main - Entry point
+   * Description: 'The program prints a string followed by a new line'
+   * Return: Always 0
+   */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
