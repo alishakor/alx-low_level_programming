241d007fb6a3f033ca6d065059d964717e0101c2
alishakor <aliuokeowo@gmail.com>
2022-10-25 14:18:09 +0000

    Check for uppercase character

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistad>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
