db164b2f13baa0a88f84584b2698ada100fbd6af
alishakor <aliuokeowo@gmail.com>
2022-12-06 11:24:37 +0000

    a program that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a test file and prints it to the
- *                POSIX standard output
- * @filename: name of the file we want to read from
- * @letter: numbers of letters it should read and print
+ * read_textfile -  reads a text file and prints it to
+ *                  the POSIX standard output
+ * @filename: name of the file we want to read from.
+ * @letters: numbers of letters it should read and print.
  *
- * Return: actual number of letters it could read and print
- *         if the file can not be opened or read, return 0
- *	   if filename is NULL return 0
- *	   if write fails returns 0
+ * Return: actual number of letters it could read and print or
+ *         if the file can not be opened or read, return 0 or
+ *         if filename is NULL return 0 or
+ *         if write fails or does not write the expected
+ *         amount of bytes, return 0
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, r, w;
 	char *buff;
@@ -24,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 	if (buff == NULL)
 		return (0);
 
-	fd = open(filename, O_CREAT | 0_RDONLY);
+	fd = open(filename, O_CREAT | O_RDONLY);
 
 	if (fd == -1)
 	{
@@ -52,4 +54,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 	close(fd);
 
 	return (w);
+
 }
